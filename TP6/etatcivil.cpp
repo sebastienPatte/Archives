bfb86ccf868abbc45f5f7b0870c7c5f245937c55
sebastienPatte <35399281+sebastienPatte@users.noreply.github.com>
2018-03-16 01:00:08 +0100

    update tp6

--- TP6/etatcivil.cpp
+++ TP6/etatcivil.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int MAXPERSONNES = 50;
-
+string Tab="	";
 enum Genre { M, F };
 
 struct Personne{
@@ -59,19 +59,19 @@ void AfficheEtatCivil(EtatCivil EC){
 void RemplitEtatCivil(EtatCivil &EC) { // remplit l'état civil avec 20 personnes
   AjoutPersonne("Nom0", F, -1, -1, EC);
   AjoutPersonne("Nom1", M, -1, -1, EC);
-  AjoutPersonne("Nom2", M, -1, -1, EC);
+  AjoutPersonne("Nom2", M, 	-1, -1, EC);
   AjoutPersonne("Nom3", F, -1, -1, EC);
-  AjoutPersonne("Nom4", M, 0, -1, EC);
+  AjoutPersonne("Nom4", M, 0, -1, EC);   //-------- 
   AjoutPersonne("Nom5", F, -1, 1, EC);
   AjoutPersonne("Nom6", M, 3, 2, EC);
-  AjoutPersonne("Nom7", F, 5, 4, EC);
-  AjoutPersonne("Nom8", F, 7, 6, EC);
-  AjoutPersonne("Nom9", F, 5, 10, EC);
-  AjoutPersonne("Nom10", M, 12, 11, EC);
+  AjoutPersonne("Nom7", F, 5, 4, EC);   //-------- a cause du 5
+  AjoutPersonne("Nom8", F, 7, 6, EC); 
+  AjoutPersonne("Nom9", F, 5, 10, EC);   //-------- a cause du 5 et 10
+  AjoutPersonne("Nom10", M, 12, 11, EC);   //-------- ses deux parents n on aucun parent
   AjoutPersonne("Nom11", M, -1, -1, EC);
   AjoutPersonne("Nom12", F, -1, -1, EC);
-  AjoutPersonne("Nom13", M, 9, 14, EC);
-  AjoutPersonne("Nom14", M, 16, 15, EC);
+  AjoutPersonne("Nom13", M, 9, 14, EC);   //-------- a cause du 10
+  AjoutPersonne("Nom14", M, 16, 15, EC);   //--------
   AjoutPersonne("Nom15", M, -1, 17, EC);
   AjoutPersonne("Nom16", F, 19, 18, EC);
   AjoutPersonne("Nom17", M, -1, -1, EC);
@@ -80,17 +80,59 @@ void RemplitEtatCivil(EtatCivil &EC) { // remplit l'état civil avec 20 personne
 }
 
 int cherchePersonne(string nom, EtatCivil &EC){
-    for (int i=0;i<=EC.nbP;i++){
+    for (int i=0;i<EC.nbP;i++){
         if(nom==EC.tableP[i].nom){
             return i;
         }
     }return -1;
 }
 
-void mariage(string nom1, string nom2, EtatCivil &EC){
-    if((cherchePersonne(nom1)==-1)&&(cherchePersonne(nom2)==-1){
-    
+bool mariage(string nom1, string nom2, EtatCivil &EC){
+    int ind1=cherchePersonne(nom1,EC);
+    int ind2=cherchePersonne(nom2,EC);
+    if((ind1!=-1)&&(ind2!=-1)&&(EC.tableP[ind1].indConjoint==0)&&(EC.tableP[ind2].indConjoint==0)){
+		EC.tableP[ind1].indConjoint=1;
+		EC.tableP[ind2].indConjoint=1;
+		return true;
+    }else{
+    		return false;
     }
+}
+
+
+void afficheparents(int ind,int nbTab,EtatCivil &EC){
+				
+				if(EC.tableP[ind].indParent1 >= 0) {
+					for(int i=0;i<=nbTab;i++)cout<<Tab;
+					cout<<"Individu "<<EC.tableP[ind].indParent1<<endl;
+					nbTab++;
+						afficheparents(EC.tableP[ind].indParent1,nbTab,EC);
+					
+				}else{
+					for(int i=0;i<=nbTab;i++)cout<<Tab;
+					cout<<"Individu Inconnu"<<endl;
+				}
+			
+		
+			
+				if(EC.tableP[ind].indParent2 >=0) {
+					for(int i=0;i<=nbTab;i++)cout<<Tab;
+					cout<<"Individu "<<EC.tableP[ind].indParent2<<endl;
+					nbTab++;
+					afficheparents(EC.tableP[ind].indParent2,nbTab,EC);
+					
+				}else{
+					for(int i=0;i<=nbTab;i++)cout<<Tab;
+					cout<<"Individu Inconnu"<<endl;
+					
+				}
+
+}
+
+void arbreGenealogique(string nom, EtatCivil &EC){
+	int ind=cherchePersonne(nom,EC);
+	cout<<"Individu "<<ind<<endl;
+	afficheparents(ind,0,EC);
 }
 
 int main() {
@@ -99,5 +141,7 @@ int main() {
   RemplitEtatCivil(EC);
   AfficheEtatCivil(EC);
   cout<<cherchePersonne("Nom9",EC)<<endl;
-  
+// cout<<mariage("Nom1","Nom2",EC)<<endl;
+// cout<<mariage("Nom2","Nom4",EC)<<endl;
+ arbreGenealogique("Nom9",EC); 
 }
