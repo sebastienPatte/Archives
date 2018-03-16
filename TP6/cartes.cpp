849df30719e12fb8bcc39f88bd5701570b4d6fe1
sebastienPatte <35399281+sebastienPatte@users.noreply.github.com>
2018-03-16 15:03:39 +0100

    Add files via upload

--- TP6/cartes.cpp
+++ TP6/cartes.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
 enum Couleur { P, Co, T, Ca };
 
 struct Carte {
@@ -9,6 +13,14 @@ struct Paquet {
   int taille;
   Carte carte[40];
 };
+
+//prend en paramètre min et max et renvoie un chiffre aléàtoire entre min et max
+int randint( int min, int max ) {
+	return min + rand() % ( max - min );
+	/* 'rand()' renvoie un chiffre entre 0 et 'temps écoulé depuis le lancement' ( voir première ligne du main() ) . 
+	    on prend modulo ( 'max' - 'min' )  pour avoir un chiffre entre 0 et la différence entre 'max'et 'min' .
+	    on ajoute le minimum pour être sur d'avoir au moins 'min' . */
+}
 
 void initPaquet (Paquet &paquet) {
   paquet.taille=40;
@@ -24,4 +36,47 @@ void initPaquet (Paquet &paquet) {
       paquet.carte[4*i+3].valeur = i+1;
       paquet.carte[4*i+3].couleur = Ca;
     }
+}
+
+void affichePaquet(Paquet paquet){
+	for(int i=0; i<paquet.taille;i++){
+		cout<< paquet.carte[i].valeur <<" ";
+		switch(paquet.carte[i].couleur){
+			case 0: cout<<"Pique"<<endl;
+				   break; 
+			case 1: cout<<"Coeur"<<endl;
+				   break;
+			case 2: cout<<"Trefle"<<endl;
+				   break;
+			case 3: cout<<"Carreau"<<endl;
+				   break; 			
+		}
+	}
+	cout<< endl;
+}
+
+void permuter(int ind1, int ind2, Paquet &paquet){
+	Carte carteTemp= paquet.carte[ind1];
+	paquet.carte[ind1]=paquet.carte[ind2];
+	paquet.carte[ind2]=carteTemp;
+}
+
+
+void melangePaquet(Paquet &paquet){
+	int ind1,ind2;
+	
+	for(int i=0; i<paquet.taille; i++){
+		ind1=randint(0,39);
+		ind2=randint(0,39);
+		permuter(ind1,ind2, paquet);
+	}
+	
+}
+
+int main(){
+	Paquet paquet;
+	initPaquet(paquet);
+	affichePaquet(paquet);
+	melangePaquet(paquet);
+	affichePaquet(paquet);
 }
--- TP6/etatcivil.cpp
+++ TP6/etatcivil.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int MAXPERSONNES = 50;
-string Tab="	";
+
 enum Genre { M, F };
 
 struct Personne{
@@ -31,7 +31,7 @@ void AjoutPersonne(string sonNom, Genre s, int parent1, int parent2, EtatCivil &
         personne.indParent1=parent1;
         personne.indParent2=parent2;
         personne.indConjoint=0;
-        EC.tableP[EC.nbP+1]=personne;
+        EC.tableP[EC.nbP]=personne;
         EC.nbP++;
     }else{
         cout<<"Erreur lors de l'ajout de la personne"<<endl;
@@ -61,27 +61,27 @@ void RemplitEtatCivil(EtatCivil &EC) { // remplit l'état civil avec 20 personne
   AjoutPersonne("Nom1", M, -1, -1, EC);
   AjoutPersonne("Nom2", M, 	-1, -1, EC);
   AjoutPersonne("Nom3", F, -1, -1, EC);
-  AjoutPersonne("Nom4", M, 0, -1, EC);   //-------- 
+  AjoutPersonne("Nom4", M, 0, -1, EC);   
   AjoutPersonne("Nom5", F, -1, 1, EC);
   AjoutPersonne("Nom6", M, 3, 2, EC);
-  AjoutPersonne("Nom7", F, 5, 4, EC);   //-------- a cause du 5
-  AjoutPersonne("Nom8", F, 7, 6, EC); 
-  AjoutPersonne("Nom9", F, 5, 10, EC);   //-------- a cause du 5 et 10
-  AjoutPersonne("Nom10", M, 12, 11, EC);   //-------- ses deux parents n on aucun parent
+  AjoutPersonne("Nom7", F, 5, 4, EC);   
+  AjoutPersonne("Nom8", F, 7, 6, EC);
+  AjoutPersonne("Nom9", F, 5, 10, EC);   
+  AjoutPersonne("Nom10", M, 12, 11, EC);
   AjoutPersonne("Nom11", M, -1, -1, EC);
   AjoutPersonne("Nom12", F, -1, -1, EC);
-  AjoutPersonne("Nom13", M, 9, 14, EC);   //-------- a cause du 10
-  AjoutPersonne("Nom14", M, 16, 15, EC);   //--------
+  AjoutPersonne("Nom13", M, 9, 14, EC);
+  AjoutPersonne("Nom14", M, 16, 15, EC);
   AjoutPersonne("Nom15", M, -1, 17, EC);
   AjoutPersonne("Nom16", F, 19, 18, EC);
   AjoutPersonne("Nom17", M, -1, -1, EC);
   AjoutPersonne("Nom18", M, -1, -1, EC);
-  AjoutPersonne("Nom19", F, -1, -1, EC);
+  AjoutPersonne("Nom19", F, 3, -1, EC);
 }
 
 int cherchePersonne(string nom, EtatCivil &EC){
     for (int i=0;i<EC.nbP;i++){
-        if(nom==EC.tableP[i].nom){
+        if(EC.tableP[i].nom == nom){
             return i;
         }
     }return -1;
@@ -100,39 +100,33 @@ bool mariage(string nom1, string nom2, EtatCivil &EC){
 }
 
 
-void afficheparents(int ind,int nbTab,EtatCivil &EC){
-				
-				if(EC.tableP[ind].indParent1 >= 0) {
-					for(int i=0;i<=nbTab;i++)cout<<Tab;
-					cout<<"Individu "<<EC.tableP[ind].indParent1<<endl;
-					nbTab++;
-						afficheparents(EC.tableP[ind].indParent1,nbTab,EC);
-					
-				}else{
-					for(int i=0;i<=nbTab;i++)cout<<Tab;
-					cout<<"Individu Inconnu"<<endl;
-				}
-			
-		
-			
-				if(EC.tableP[ind].indParent2 >=0) {
-					for(int i=0;i<=nbTab;i++)cout<<Tab;
-					cout<<"Individu "<<EC.tableP[ind].indParent2<<endl;
-					nbTab++;
-					afficheparents(EC.tableP[ind].indParent2,nbTab,EC);
-					
-				}else{
-					for(int i=0;i<=nbTab;i++)cout<<Tab;
-					cout<<"Individu Inconnu"<<endl;
-					
-				}
 
-}
 
-void arbreGenealogique(string nom, EtatCivil &EC){
-	int ind=cherchePersonne(nom,EC);
-	cout<<"Individu "<<ind<<endl;
-	afficheparents(ind,0,EC);
+//------------------------------------------------------------------------------------
+void AfficheParents(int ind, EtatCivil EC, int nbTab) {
+	
+	int i = 0;
+	cout << "Individu ";
+	if (ind == -1) {
+		cout << "inconnu" << endl;
+	}else{
+			cout << ind << endl;
+			for (i=0; i < nbTab; i++)cout <<"    ";
+			AfficheParents(EC.tableP[ind].indParent1, EC, nbTab+1);
+			for (i=0; i < nbTab; i++)cout <<"    ";
+			AfficheParents(EC.tableP[ind].indParent2, EC, nbTab+1);
+		}
+} 
+	
+
+void AfficheArbreGenePersonne(int ind, EtatCivil EC) {
+	
+	int nbTab = 1;
+	if (ind >= EC.nbP){
+		cout << "La personne d'indice " << ind << " n existe pas." << endl;
+	}else{
+		AfficheParents(ind, EC, nbTab);
+	}
 }
 
 int main() {
@@ -143,5 +137,6 @@ int main() {
   cout<<cherchePersonne("Nom9",EC)<<endl;
 // cout<<mariage("Nom1","Nom2",EC)<<endl;
 // cout<<mariage("Nom2","Nom4",EC)<<endl;
- arbreGenealogique("Nom9",EC); 
+// arbreGenealogique("Nom9",EC); 
+AfficheArbreGenePersonne(8,EC);
 }
