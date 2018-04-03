3984a0caf6d7e35c9add99ba78fc5d7ca6b7a3a9
sebastienPatte <sebnb91@gmail.com>
2018-04-03 14:48:07 +0200

    [projet] debut init pheromones

--- projet/projet.cpp
+++ projet/projet.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define SHOW_EMPTY_PLACES false
+#define SHOW_EMPTY_PLACES true
 #define SHOW_PRINTS false
+
+
 
 
 
@@ -20,6 +22,7 @@ struct place{
 	int indiceFourmi;
 	bool sucre;
 	bool nid;
+	float pheromoneNid;
 };
 
 typedef vector< vector<place> > Grille;
@@ -33,6 +36,11 @@ int randint( int min, int max ) {
 	    on ajoute le minimum pour être sur d'avoir au moins 'min' . */
 }
 
+int max(int nb1,int  nb2){
+	if(nb1>nb2)return nb1;
+	else return nb2;
+}
+
 void creerFourmi(int indiceFourmi, coord positionFourmi, vector<Fourmi> &tabFourmis, Grille &T){
 	Fourmi f;
 	f.coordonnees=positionFourmi;
@@ -44,17 +52,70 @@ void creerFourmi(int indiceFourmi, coord positionFourmi, vector<Fourmi> &tabFour
 	T[f.coordonnees.X][f.coordonnees.Y].indiceFourmi= indiceFourmi;
 }
 
+vector<coord> voisins(coord coordonnees,Grille T){
 
+	vector<coord> res(4);			
+	res[0]= {coordonnees.X+1,coordonnees.Y};
+	res[1]= {coordonnees.X,coordonnees.Y+1};
+	res[2]= {coordonnees.X-1,coordonnees.Y};
+	res[3]= {coordonnees.X,coordonnees.Y-1};
+
+
+	return res;
+		    
+}
+
+bool dansGrille(coord res, Grille T){
+
+	if((res.X>=T.size()) or (res.Y>=T.size()))return false;
+	if ((res.X<0) or (res.Y<0 ))return false;
+	return true;
+	
+}
+
+int maxPheromoneNidVoisins(int i, int j, Grille T){
+	coord coordonnees={i,j};
+	vector<coord> coord_voisins= voisins(coordonnees,T);
+	int res=0;
+
+	for (int i=0; i<coord_voisins.size(); i++){
+		if(dansGrille(coord_voisins[i], T)){
+			res=max(res,T[coord_voisins[i].X][coord_voisins[i].Y].pheromoneNid);
+		}
+	}
+	return res;
+
+}
+void initPheromonesNid(Grille &T){
+	// INIT PHEROMONES NID
+		T[T.size()/2][T.size()/2].pheromoneNid= 1;
+		T[(T.size()/2)-1][T.size()/2].pheromoneNid= 1;
+		T[T.size()/2][(T.size()/2)-1].pheromoneNid= 1;
+		T[(T.size()/2)-1][(T.size()/2)-1].pheromoneNid= 1;
+
+		for(int i=0; i<T.size(); i++){
+			for(int j=0; j<T.size(); j++){
+
+				if(T[i][j].pheromoneNid <1){
+					T[i][j].pheromoneNid = max(maxPheromoneNidVoisins(i,j,T)-1/T.size(),0);
+					
+				}
+			}
+		}
+}
 	
 
 void initPlateau(int nbSucres,vector<Fourmi> &tabFourmis,Grille &T){
 	int zoneSucres,posSucreX,posSucreY;
+	// INIT GRILLE
 	for(int i=0;i<T.size();i++){
 		T[i].resize(T.size());
 		for(int j=0;j<T[i].size();j++){
 			T[i][j]= {-1,false,false};
 		}
 	}
+
+	// INIT SUCRE
 	zoneSucres=randint(0,10);
 	int k=0;
 	do{
@@ -65,14 +126,15 @@ void initPlateau(int nbSucres,vector<Fourmi> &tabFourmis,Grille &T){
 			k++;
 		}
 	}while(k<nbSucres);
-	
+	// INIT NID
 	T[T.size()/2][T.size()/2].nid= true;
 	T[T.size()/2-1][T.size()/2].nid= true;
 	T[T.size()/2][T.size()/2-1].nid= true;
 	T[T.size()/2-1][T.size()/2-1].nid= true;
-	
+
+
 	int indiceFourmi=0;
-	
+	// INIT FOURMIS
 	coord positionFourmi;
 	for(int i=(T.size()/2)-2 ; i<=(T.size()/2)+1 ; i++){
 		for(int j=(T.size()/2)-2; j<=(T.size()/2)+1; j++){
@@ -84,13 +146,14 @@ void initPlateau(int nbSucres,vector<Fourmi> &tabFourmis,Grille &T){
 			}
 		}
 	}
-	
+	initPheromonesNid(T);	
 }
 
 
 
+
 void afficheGrille(vector<Fourmi> &tabFourmis, Grille T){
-	for(int c=0;c<42;c++)cout<<"#";
+	for(int c=0;c<T.size()*2+2;c++)cout<<"#";
 	cout<<endl;
 	for (int i=0; i<T.size(); i++){
 		cout<<"#";
@@ -111,7 +174,7 @@ void afficheGrille(vector<Fourmi> &tabFourmis, Grille T){
 						cout<<"N";
 					}else{
 						if (SHOW_EMPTY_PLACES) {
-							cout<<"-";
+							cout<<T[i][j].pheromoneNid;
 						} else {
 							cout<<" ";
 						}
@@ -120,7 +183,7 @@ void afficheGrille(vector<Fourmi> &tabFourmis, Grille T){
 			}
 		}cout<<"#"<<endl;
 	}
-	for(int c=0;c<42;c++)cout<<"#";
+	for(int c=0;c<T.size()*2+2;c++)cout<<"#";
 	cout<<endl;
 }
 
@@ -140,13 +203,7 @@ bool placeVide(place p){
 	return ((p.indiceFourmi==-1)&&(!p.sucre)&&(!p.nid));
 }
 
-bool dansGrille(coord res, Grille T){
 
-	if((res.X>=T.size()) or (res.Y>=T.size()))return false;
-	if ((res.X<0) or (res.Y<0 ))return false;
-	return true;
-	
-}
 
 coord choisiCoordAleatoirement(vector<coord> tabCoord, Grille T){
 	coord res;
@@ -158,38 +215,34 @@ coord choisiCoordAleatoirement(vector<coord> tabCoord, Grille T){
 }
 
 
-vector<coord> voisins(Fourmi f,Grille T){
 
-	vector<coord> res(4);			
-	res[0]= {f.coordonnees.X+1,f.coordonnees.Y};
-	res[1]= {f.coordonnees.X,f.coordonnees.Y+1};
-	res[2]= {f.coordonnees.X-1,f.coordonnees.Y};
-	res[3]= {f.coordonnees.X,f.coordonnees.Y-1};
-
-
-	return res;
-		    
-}
-
-bool sucreVoisin(Fourmi &f, vector<Fourmi> tabFourmis, Grille &T){
+bool scanneVoisin(Fourmi &f, vector<Fourmi> tabFourmis, Grille &T){
 	
-	vector<coord> coord_voisins= voisins(f,T);
+	vector<coord> coord_voisins= voisins(f.coordonnees,T);
 	
 	for(int i=0;i<coord_voisins.size();i++){
-		if( dansGrille(coord_voisins[i],T) && T[coord_voisins[i].X][coord_voisins[i].Y].sucre){
-			T[coord_voisins[i].X][coord_voisins[i].Y].sucre=false;
-			f.porteSucre=true;
-			tabFourmis[T[f.coordonnees.X][f.coordonnees.Y].indiceFourmi]=f;
-			return true;
+		if(dansGrille(coord_voisins[i],T)){
+			if( T[coord_voisins[i].X][coord_voisins[i].Y].sucre  && f.porteSucre==false){
+				T[coord_voisins[i].X][coord_voisins[i].Y].sucre=false;
+				f.porteSucre=true;
+				tabFourmis[T[f.coordonnees.X][f.coordonnees.Y].indiceFourmi]=f;
+				return true;
+			}
+			if( T[coord_voisins[i].X][coord_voisins[i].Y].nid && f.porteSucre==true){
+				f.porteSucre=false;
+				tabFourmis[T[f.coordonnees.X][f.coordonnees.Y].indiceFourmi]=f;
+				return true;
+			}
 		}
 	}return false;
 }
 
+
 void unTour(vector<Fourmi> &tabFourmis , Grille &T){
 	coord newCoord;
 	for(int i=0;i<tabFourmis.size();i++){
-		if(!sucreVoisin(tabFourmis[i], tabFourmis, T)){
-			newCoord=choisiCoordAleatoirement(voisins(tabFourmis[i],T),T);
+		if(!scanneVoisin(tabFourmis[i], tabFourmis, T)){
+			newCoord=choisiCoordAleatoirement(voisins(tabFourmis[i].coordonnees,T),T);
 			if(SHOW_PRINTS)printf("MOVE ANT TO COORDS : %d, %d\n", newCoord.X, newCoord.Y);
 			deplaceFourmi(tabFourmis[i], T[tabFourmis[i].coordonnees.X][tabFourmis[i].coordonnees.Y], T[newCoord.X][newCoord.Y], newCoord, tabFourmis );
 		}
@@ -204,7 +257,7 @@ int main(){
 	int nbSucres=5;
 	coord positionFourmi={2,1};
 	vector<Fourmi> tabFourmis(nbFourmis);
-	
+		
 	Grille T(tailleCarte);
 	initPlateau(nbSucres,tabFourmis,T);
 	afficheGrille(tabFourmis,T);
@@ -219,6 +272,7 @@ int main(){
 	for(int i=0; i<tabFourmis.size();i++) {
 		if(SHOW_PRINTS)cout<<tabFourmis[i].coordonnees.X<<", "<<tabFourmis[i].coordonnees.Y<<endl;
 	}
+	
 /*
 	//tests deplacement de fourmi
 	coord coord_p2={5,3};
