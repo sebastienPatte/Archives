6abdc4d752a57b2f55ba4af966e82c3fadda5753
sebastienPatte <sebnb91@gmail.com>
2018-11-04 21:00:00 +0100

    finish train and test 50% errors

--- Programmes_java/ProjetIA/src/image/LoadImage.java
+++ Programmes_java/ProjetIA/src/image/LoadImage.java
@@ -2,12 +2,23 @@ package image;  // mettre le nom du package approprie
  
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+//import perceptron.OnlinePerceptron;
 import caltech.CalTech101;
  
  
 public class LoadImage {
 	
-	
+	public static int maxT (double[] T, int taille){
+		int res = 0;
+		double tmp=0;
+		for (int cpt=0; cpt<taille; cpt++){
+			if(tmp<=T[cpt]){
+				tmp=T[cpt];
+				res=cpt;
+			}
+		}return res;
+	}
 
 	public static double scalaire(double[]x, double[] w){
     	double result=0;
@@ -16,7 +27,16 @@ public class LoadImage {
     	}return result;
     }
 	
-
+	public static int score(double[][] data, int[] refs, int prediction){
+		int nbErr = 0;
+		for (int i = 0; i < data.length; i++) {
+			if(prediction*refs[i] <=0){
+				nbErr++;
+			}
+		}
+		return nbErr;
+	}
+	
 	/*
 	public static float epoque(double[] data, double[] w, int label, int DIM){
 
@@ -84,32 +104,43 @@ public class LoadImage {
 		double w[][]= new double[101][785];
 		for(int i=0; i<w.length; i++){
 			for(int j=0; j<w[i].length;j++){
-				w[i][j]=1;
+				w[i][j]=0.5;
 			}
 			
 		}
 		// charger toutes les donnees d'apprentissage (train)
-		double[][] trainIm = new double[4100][(28*28)+1];
+		double[][] trainIm = new double[4100][784];
+		double[][] trainImB = new double[4100][785];
 		int[] trainRefs = new int[4100];
 		double[] predictions= new double [101];
-		for(int i=0; i<2; i++) {   //i<4100
+		for(int i=0; i<4100; i++) {   //i<4100
 			trainRefs[i] = CT.getTrainLabel(i);
 			trainIm[i] = CT.getTrainImage(i);
 			
 			//ajout du biais 
-			
-			trainIm[i][trainIm[i].length-1]=1;
+			for(int cptTrainIm=0; cptTrainIm <=784; cptTrainIm++){
+				//biais à la fin
+				if(cptTrainIm==784){
+					trainImB[i][784]=1;
+					
+				}else{
+				//recuperation de trainIm
+				trainImB[i][cptTrainIm]=trainIm[i][cptTrainIm]; 
+				}
+			}
 			
 			//init Tlabel
 			float[] Tlabel= new float [101];
 			for (int cptlabel=0; cptlabel<Tlabel.length; cptlabel++){
 				Tlabel[cptlabel]=0;
-			}Tlabel[trainRefs[i]]=1;
+			}Tlabel[trainRefs[i]-1]=1;
 			
 			//apprentissage
 			for(int j=0; j<w.length;j++){
-				predictions[j]=prediction(w[j],trainIm[i]);
-				//System.out.println(predictions[j]);	
+				predictions[j]=prediction(w[j],trainImB[i]);
+				//System.out.println("predictions["+j+"] = "+predictions[j]);
+				
+				
 			}
 			//softmax
 			predictions=softmax(predictions);
@@ -117,24 +148,56 @@ public class LoadImage {
 			double Stest=0;
 			for(int test=0; test<predictions.length;test++){
 				Stest+=predictions[test];
+				//System.out.println("predictions["+test+"] = "+predictions[test]);
 			}
 			
 			//MAJ du poids
 			for(int k=0; k<w.length;k++){
-				for(int l=0; l<w[k].length-1;l++){
-					w[k][l]=  (w[k][l] + (0.7  * trainIm[i][l] * (Tlabel[k] - predictions[k])));
-					System.out.println("w["+ k +"]["+ l +"] =  "+w[k][l]);
+				for(int l=0; l<w[k].length;l++){
+					w[k][l]+=   (0.25  * trainImB[i][l]* ( Tlabel[k] - predictions[k]));
+					
+					//System.out.println("w["+ k +"]["+ l +"] =  "+w[k][l]);
 				}
-				 
+				//System.out.println(( predictions[k]));
+				
 			}
+			System.out.println("prediction["+maxT(predictions,101)+"] = "+predictions[maxT(predictions,101)]);
+			System.out.println("image "+i);
 			System.out.println("SommePredicts = "+ Stest);
 			//System.out.println(trainIm[i][i] + " "+ trainRefs[i]);
-			System.out.println(trainIm[0].length);
-		}
+			//System.out.println("biais : trainImB["+ i +"][784] = "+trainImB[i][784]);
+			
+			//reinitialisation des parametres
+			for(int cptPredict=0;cptPredict<predictions.length;cptPredict++){
+				predictions[cptPredict]=0;
+			}
+			Stest=0;
+			
+		}//fin boucle Train
 		//image2VecteurReel_withB(trainIm[0]);
 		
-		
 		// Pour les donnees de test, utiliser  CT.getTestLabel  et  CT.getTestImage
+		int nbErr=0;
+		double[][] testIm = new double [2000][784];
+		int[] testRefs = new int [2000];
+		int prediction = 0;
+		for (int cptTest=0; cptTest<testIm.length;cptTest++){
+			//chargement des images et labels de test
+			testIm[cptTest]=CT.getTestImage(cptTest);
+			testRefs[cptTest]=CT.getTestLabel(cptTest);
+			double[] predictionsTest= new double [101];
+			//TEST
+			for(int cptTestClasse=0; cptTestClasse<w.length; cptTestClasse++){
+				predictionsTest[cptTestClasse]= prediction(w[cptTestClasse],testIm[cptTest]);
+			}
+			predictionsTest=(softmax(predictionsTest));
+			prediction=maxT(predictionsTest,101)+1; //on ajoute 1 car maxT renvoie l'indice du tableau (qui commence à 0) alors que le nb de classes commence à 1
+			System.out.println("prediction "+cptTest+" = "+prediction+" label = "+testRefs[cptTest]);
+			if(prediction!=testRefs[cptTest])nbErr++;
+			
+		}
+		System.out.println(nbErr);
+		
 		
 	}
 }
