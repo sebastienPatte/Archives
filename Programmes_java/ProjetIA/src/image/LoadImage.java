188db8abf4779b8d327e53593a0ab429b96251a0
sebastienPatte <sebnb91@gmail.com>
2018-11-06 21:00:27 +0100

    optimisations softmax, 57,7% de taux de reussite sur les tests

--- Programmes_java/ProjetIA/src/image/LoadImage.java
+++ Programmes_java/ProjetIA/src/image/LoadImage.java
@@ -2,7 +2,6 @@ package image;  // mettre le nom du package approprie
  
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 //import perceptron.OnlinePerceptron;
 import caltech.CalTech101;
  
@@ -67,7 +66,7 @@ public class LoadImage {
 		//}
 		
 	public static double prediction(double[] w, double[] x){
-		double prediction= scalaire(x,w);
+		double prediction= scalaire(x,w)*0.001;
 		prediction = Math.exp(prediction);
 		return prediction;
 	}
@@ -83,6 +82,22 @@ public class LoadImage {
 		return predicts;
 	}
 	
+	public static void maxDecroissant(double[] tauxErreur){
+		double[][] res= new double[101][101];
+		double tmp=0;
+		int tmpi =0;
+		for(int cpt=0; cpt<res.length;cpt++){	
+			for(int i=0; i<tauxErreur.length;i++){
+					if(tmp<=tauxErreur[i]){
+						tmp=tauxErreur[i];
+						tmpi=i;
+					}
+			}tauxErreur[tmpi]=0;
+//			if(tmp!=0)System.out.println("classe "+(tmpi+1)+" = "+tmp);
+			tmp=0;
+			tmpi=0;
+		}
+	}
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		System.out.println("Test load images");
                 String path="/home/seb/Bureau/Git/Java/IA/"; //  indiquer le path correct
@@ -108,14 +123,24 @@ public class LoadImage {
 			}
 			
 		}
+		
+		double[] ErrClass= new double [101];
+		double[] BonneClass = new double [101];
+		double[] ErrClassT= new double [101];
+		double[] BonneClassT = new double [101];
+		
 		// charger toutes les donnees d'apprentissage (train)
 		double[][] trainIm = new double[4100][784];
 		double[][] trainImB = new double[4100][785];
 		int[] trainRefs = new int[4100];
 		double[] predictions= new double [101];
+		int nbErr=0;
+		
+		for(int cpt=0;cpt<20;cpt++){
+		
 		for(int i=0; i<4100; i++) {   //i<4100
-			trainRefs[i] = CT.getTrainLabel(i);
-			trainIm[i] = CT.getTrainImage(i);
+				trainRefs[i] = CT.getTrainLabel(i);
+				trainIm[i] = CT.getTrainImage(i);
 			
 			//ajout du biais 
 			for(int cptTrainIm=0; cptTrainIm <=784; cptTrainIm++){
@@ -152,32 +177,61 @@ public class LoadImage {
 			}
 			
 			//MAJ du poids
-			for(int k=0; k<w.length;k++){
-				for(int l=0; l<w[k].length;l++){
-					w[k][l]+=   (0.25  * trainImB[i][l]* ( Tlabel[k] - predictions[k]));
+			int prediction=maxT(predictions,101);
+			if(prediction!=trainRefs[i]-1){	
+				ErrClassT[prediction]++;
+				nbErr++;
+				for(int k=0; k<w.length;k++){
+					for(int l=0; l<w[k].length;l++){
 					
-					//System.out.println("w["+ k +"]["+ l +"] =  "+w[k][l]);
-				}
-				//System.out.println(( predictions[k]));
+						w[k][l]+=   (0.8  * trainImB[i][l]* ( Tlabel[k] - predictions[k]));
+					
+						//System.out.println("w["+ k +"]["+ l +"] =  "+w[k][l]);
+					}
+					//System.out.println(( predictions[k]));
 				
+				}
+			}else{
+				BonneClassT[prediction]++;
 			}
-			System.out.println("prediction["+maxT(predictions,101)+"] = "+predictions[maxT(predictions,101)]);
-			System.out.println("image "+i);
-			System.out.println("SommePredicts = "+ Stest);
+//			System.out.println("image "+i);
+//			System.out.println("prediction["+maxT(predictions,101)+"] = "+predictions[maxT(predictions,101)]);
+//			System.out.println("SommePredicts = "+ Stest);
 			//System.out.println(trainIm[i][i] + " "+ trainRefs[i]);
 			//System.out.println("biais : trainImB["+ i +"][784] = "+trainImB[i][784]);
-			
+
 			//reinitialisation des parametres
 			for(int cptPredict=0;cptPredict<predictions.length;cptPredict++){
 				predictions[cptPredict]=0;
 			}
-			Stest=0;
-			
+			Stest=0;	
 		}//fin boucle Train
+		
+		if(cpt!=0){
+			System.out.println("nbErr["+cpt+"] = "+nbErr/cpt);	
+		}else{
+			System.out.println("nbErr["+cpt+"] = "+nbErr);
+		}
+		}//fin boucle de boucles train
 		//image2VecteurReel_withB(trainIm[0]);
+		double[] tauxErrT= new double [101];
+		for(int cptRes=0;cptRes<ErrClass.length;cptRes++){
+			//System.out.println("Erreurs classe "+cptRes+" = "+ErrClass[cptRes]);
+			//System.out.println("Bonne classe "+cptRes+" = "+BonneClass[cptRes]);
+			if((BonneClassT[cptRes]+ErrClassT[cptRes])!=0){
+				System.out.println("taux d'Erreur classe "+(cptRes+1)+" = "+100*(ErrClassT[cptRes])/ (BonneClassT[cptRes]+ErrClassT[cptRes])+"%  "+(BonneClassT[cptRes]+ErrClassT[cptRes])+" image(s)");
+				tauxErrT[cptRes]=100*(ErrClass[cptRes])/ (BonneClassT[cptRes]+ErrClassT[cptRes]);
+			}else{
+				System.out.println("taux d'Erreur classe "+(cptRes+1)+" =  0 car aucune image n'est de cette classe");
+				tauxErrT[cptRes]=0;
+			}
+		}
+		maxDecroissant(tauxErrT);
+		
 		
 		// Pour les donnees de test, utiliser  CT.getTestLabel  et  CT.getTestImage
-		int nbErr=0;
+		System.out.println("nbErr "+nbErr);
+		nbErr=0;
 		double[][] testIm = new double [2000][784];
 		int[] testRefs = new int [2000];
 		int prediction = 0;
@@ -192,10 +246,29 @@ public class LoadImage {
 			}
 			predictionsTest=(softmax(predictionsTest));
 			prediction=maxT(predictionsTest,101)+1; //on ajoute 1 car maxT renvoie l'indice du tableau (qui commence à 0) alors que le nb de classes commence à 1
-			System.out.println("prediction "+cptTest+" = "+prediction+" label = "+testRefs[cptTest]);
-			if(prediction!=testRefs[cptTest])nbErr++;
+//			System.out.println("prediction "+cptTest+" = "+prediction+" label = "+testRefs[cptTest]);
+			if(prediction!=testRefs[cptTest]){
+				ErrClass[prediction-1]++;
+				nbErr++;
+			}else{
+				BonneClass[prediction-1]++;
+			}
 			
+			
+		}//fin boucle TEST
+		double[] tauxErr= new double [101];
+		for(int cptRes=0;cptRes<ErrClass.length;cptRes++){
+			//System.out.println("Erreurs classe "+cptRes+" = "+ErrClass[cptRes]);
+			//System.out.println("Bonne classe "+cptRes+" = "+BonneClass[cptRes]);
+			if((BonneClass[cptRes]+ErrClass[cptRes])!=0){
+//				System.out.println("taux d'Erreur classe "+(cptRes+1)+" = "+100*(ErrClass[cptRes])/ (BonneClass[cptRes]+ErrClass[cptRes])+"%       "+(BonneClass[cptRes]+ErrClass[cptRes])+"   images");
+				tauxErr[cptRes]=100*(ErrClass[cptRes])/ (BonneClass[cptRes]+ErrClass[cptRes]);
+			}else{
+				//System.out.println("taux d'Erreur classe "+(cptRes+1)+" =  0 car aucune image n'est de cette classe");
+				tauxErr[cptRes]=0;
+			}
 		}
+		maxDecroissant(tauxErr);
 		System.out.println(nbErr);
 		
 		
